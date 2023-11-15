192475251bb7927eb79c566751853109d5a485c9
sasikirank <sasikirank@10.4.33.43>
2023-11-15 13:26:04 +0530

    sashuu

--- src/main/java/com/infinite/HelloController1.java
+++ src/main/java/com/infinite/HelloController1.java
@@ -3,6 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController1 {
+	
 @RequestMapping("/hello1")
 	public String display()
 	{
